7e51dcf6796ed15b54a571a73feb09c941ce43d1
gabantesp <40573258+gabantesp@users.noreply.github.com>
2020-07-12 21:13:05 -0400

    added fade in effect for site skeleton

--- mycode/routes.py
+++ mycode/routes.py
@@ -12,20 +12,21 @@ def index():
 
 @app.route("/experience", methods = ["GET"])
 def experience():
-	return render_template('experience.html')
+	return render_template('/experience.html')
 
 @app.route("/blog", methods = ["GET"])
 def blog():
-	return render_template('blog.html')
+	return render_template('/blog.html')
 
 @app.route("/resources", methods = ["GET"])
 def resources():
-	return render_template('resources.html')
+	return render_template('/resources.html')
 
 @app.route("/games", methods = ["GET"])
 def games():
-	return render_template('games.html')
+	return render_template('/games.html')
 
 @app.route("/music", methods = ["GET"])
 def music():
-	return render_template('music.html')
+	return render_template('/music.html')
+
