cf94966008b9ea0de9fbf0a669b5a403d1030511
gaespinoza <gabantesp@gmail.com>
2020-12-22 13:39:06 -0500

    changed blog page layout

--- mycode/__init__.py
+++ mycode/__init__.py
@@ -12,7 +12,7 @@ from flask_login import LoginManager
 
 
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
-DATABASE = 'sqliteext:///%s' % os.path.join(APP_DIR, 'blog.db')
+DATABASE_URL = 'sqliteext:///%s' % os.path.join(APP_DIR, 'blog.db')
 DEBUG = False
 SITE_WIDTH = 800
 
